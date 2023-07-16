46b5541898d4830794cf7806e4d6771f0ba084cd
Robin Syihab <robinsyihab@gmail.com>
2023-07-16 19:55:29 +0700

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,9 @@
 use anyhow::{anyhow, Error as AnyhowError};
 
-// use image::ImageError;
-
-// use serde_json::json;
 use std::io;
 
 #[derive(Debug)]
 pub struct WError(pub AnyhowError);
-
-// pub type ApiResult = Result<HttpResponse, WError>;
 
 impl std::fmt::Display for WError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -35,43 +30,3 @@ impl From<ImapCodec> for WError {
         Self(anyhow!("{:?}", err))
     }
 }
-
-// impl<T> From<actix_web::error::InternalError<T>> for WError
-// where
-//     T: std::fmt::Debug + std::fmt::Display,
-// {
-//     fn from(error: actix_web::error::InternalError<T>) -> Self {
-//         Self(anyhow!("{}", error))
-//     }
-// }
-
-// impl From<actix_web::error::Error> for WError {
-//     fn from(error: actix_web::error::Error) -> Self {
-//         Self(anyhow!("{}", error))
-//     }
-// }
-
-// impl ResponseError for WError {
-//     fn error_response(&self) -> HttpResponse {
-//         let status_code = if self.0.is::<std::io::Error>() {
-//             actix_web::http::StatusCode::INTERNAL_SERVER_ERROR
-//         } else if self.0.is::<serde_json::Error>() {
-//             actix_web::http::StatusCode::BAD_REQUEST
-//         } else {
-//             actix_web::http::StatusCode::INTERNAL_SERVER_ERROR
-//         };
-
-//         let error_json = json!({ "error": self.to_string() });
-
-//         HttpResponse::build(status_code).json(error_json)
-//     }
-// }
-
-// pub(crate) fn to_str_err(_: ToStrError) -> error::Error {
-//     error::ErrorBadRequest("Invalid UTF-8 in header value")
-// }
-
-// pub(crate) fn img_error(e: ImageError) -> error::Error {
-//     debug!("e: {}", e);
-//     error::ErrorBadRequest("Invalid image")
-// }
